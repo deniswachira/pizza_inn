d20ea7df49f236ebac1ab90ded1a601ff9fc79b6
unknown <deniswachira77@gmail.com>
2022-07-17 00:22:14 +0300

    done with pizza list

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Featured from "../components/Featured";
 import styles from '../styles/Home.module.css'
+import PizzaList from "../components/PizzaList";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Featured />
+    <PizzaList />
     </div>
   )
 }
