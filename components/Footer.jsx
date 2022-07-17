113cc2839dc4a77caaf4b766bd2d44c5298c1a27
unknown <deniswachira77@gmail.com>
2022-07-17 14:20:37 +0300

    Product page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,41 +10,37 @@ const Footer = () => {
       <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+            OH YES, WE DID IT.THE JAVA PIZZA, WELL BAKED SLICE OF PIZZA.
           </h2>
         </div>
         <div className={styles.card}>
           <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
           <p className={styles.text}>
-            1654 R. Don Road #304.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1010
+            1654 R. Moi Avenue #304.
+            <br /> Nairobi, 01000
+            <br /> (+254) 71867-1010
           </p>
           <p className={styles.text}>
-            2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1011
+            2356 K. Ronald Ngala #235.
+            <br /> Nairobi, 00100
+            <br /> (254) 7867-1011
           </p>
           <p className={styles.text}>
-            1614 E. Erwin St #104.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1012
+            1614 Waiyaki St #104.
+            <br /> Nairobi, 00100
+            <br /> (254) 7867-1012
           </p>
-          <p className={styles.text}>
-            1614 W. Caroll St #125.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1013
-          </p>
+          
         </div>
         <div className={styles.card}>
           <h1 className={styles.title}>WORKING HOURS</h1>
           <p className={styles.text}>
             MONDAY UNTIL FRIDAY
-            <br /> 9:00 – 22:00
+            <br /> 0700hrs – 2200hrs
           </p>
           <p className={styles.text}>
             SATURDAY - SUNDAY
-            <br /> 12:00 – 24:00
+            <br /> 1100hrs – 2200hrs
           </p>
         </div>
       </div>
