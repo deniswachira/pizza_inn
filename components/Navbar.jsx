82e63c9fc26e9c824e5fafddbe78467f7754ffb3
Denis Wachira <deniswachira77@gmail.com>
2022-11-18 23:26:58 +0300

    contact

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW!</div>
-          <div className={styles.text}>0712 345 678</div>
+          <div className={styles.text}>0745086753</div>
         </div>
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
+          <a href=""><li className={styles.listItem}>Homepage</li></a>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
           <Image src="/img/logo.png" alt="" width="160px" height="69px" />
